81f58333184cf3afe8594380634d3031b881aa7c
Shashidhar Sn <snshashidhar3@gmail.com>
2024-09-30 23:53:29 +0530

    made changes in header file

--- components/Header.tsx
+++ components/Header.tsx
@@ -70,7 +70,7 @@ const Header = () => (
         <NavLink href="/skills">Skills</NavLink>
       </NavItem>
       <NavItem>
-        <NavLink href="/contact">Contact</NavLink>
+        <NavLink href="/contact">Contact Me</NavLink>
       </NavItem>
     </NavList>
   </Navbar>
